1e8ebcb4130fab94e6ca54dd01e8b19b41f7e57f
alaeka <alaeka.ak80@gmail.com>
2025-10-20 12:13:23 +0700

    fyg

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <?php include "components/navbar.html" ?>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quam vel natus maiores voluptate aliquam minus nesciunt sapiente aliquid? Beatae repudiandae porro facere aperiam quis ducimus quos, blanditiis incidunt aliquam excepturi aspernatur, quaerat veritatis. Hic quas natus libero, nesciunt autem ipsam earum sit placeat voluptatum quia animi vero neque impedit voluptate iste minus repellendus odio obcaecati sint qui fugiat veniam vel delectus? Ab quam et eius praesentium quo earum quisquam repudiandae similique fugiat consequatur dolores, provident veritatis amet, est modi aliquam unde quae eum dignissimos saepe. Asperiores, sapiente accusamus quam sequi, repudiandae architecto nobis deserunt obcaecati voluptates velit perspiciatis!git </p>
 </body>
 </html>
