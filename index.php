9d8a85af9b30fc93d218a894524e47b9c4fd6549
Satria <gegedug438@gmail.com>
2025-10-28 17:04:50 +0700

    28 oct

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body class="bg-[#FFFFF]">
     <?php include "components/navbar.html" ?>
-    <h1>Ini Satria</h1>
+    <?php include "components/hero.html" ?>
+    <?php include "components/sponsor.html" ?>
+    <?php include "components/service.html" ?>
+    <?php include "components/contact.html" ?>
 </body>
 </html>
